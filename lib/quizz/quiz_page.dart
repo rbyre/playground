03f2c00b2e34075c8b506cab6e204a44ce46b6d1
rbyre <runar.byre@gmail.com>
2021-09-22 22:47:33 +0200

    added question class

--- lib/quizz/quiz_page.dart
+++ lib/quizz/quiz_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playground/quizz/question.dart';
 
 class QuizPage extends StatefulWidget {
   const QuizPage({Key? key}) : super(key: key);
@@ -10,11 +11,18 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
   int questionNumber = 0;
-  List<String> questions = [
-    'Norges høyeste fjell heter Kattnakken.',
-    'Ca. en fjerdedel av menneskets bein ligger i føttene.',
-    'En snigles blod er grønt.'
+  List<Question> questionBank = [
+    Question(questionText: 'Norges høyeste fjell heter Kattnakken.', questionAnswer: false),
+    Question(
+        questionText: 'Ca. en fjerdedel av menneskets bein ligger i føttene.',
+        questionAnswer: true),
+    Question(questionText: 'En snigles blod er grønt.', questionAnswer: false),
+    Question(
+        questionText: 'En mannlig hornbille kan løfte 850 ganger sin egen kroppsvekt.',
+        questionAnswer: true),
   ];
+
+  List<bool> answers = [false, true, false, true];
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +36,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionNumber < questions.length
-                    ? questions[questionNumber]
+                questionNumber < questionBank.length
+                    ? questionBank[questionNumber].questionText.toString()
                     : 'Ingen flere spørsmål!',
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -42,11 +50,17 @@ class _QuizPageState extends State<QuizPage> {
             child: TextButton(
                 style: TextButton.styleFrom(backgroundColor: Colors.green),
                 onPressed: () {
+                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
                   setState(() {
-                    scoreKeeper.add(Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ));
+                    correctAnswer == true
+                        ? scoreKeeper.add(Icon(
+                            Icons.check,
+                            color: Colors.green,
+                          ))
+                        : scoreKeeper.add(Icon(
+                            Icons.close,
+                            color: Colors.red,
+                          ));
                     questionNumber++;
                   });
                 },
@@ -62,13 +76,17 @@ class _QuizPageState extends State<QuizPage> {
             child: TextButton(
                 style: TextButton.styleFrom(backgroundColor: Colors.red),
                 onPressed: () {
+                  bool correctAnswer = answers[questionNumber];
                   setState(() {
-                    scoreKeeper.add(
-                      const Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ),
-                    );
+                    correctAnswer == false
+                        ? scoreKeeper.add(Icon(
+                            Icons.check,
+                            color: Colors.green,
+                          ))
+                        : scoreKeeper.add(Icon(
+                            Icons.close,
+                            color: Colors.red,
+                          ));
                     questionNumber++;
                   });
                 },
